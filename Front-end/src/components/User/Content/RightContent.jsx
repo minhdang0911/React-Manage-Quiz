0ee1ffca3a071698bab471d0e1b4bd6f47c7144c
Minh Dang <minhdang9a8@gmail.com>
2024-05-13 19:45:41 +0700

    Build a UI to display all the questions in the article, and when the user selects an answer, highlight that question.

--- Front-end/src/components/User/Content/RightContent.jsx
+++ Front-end/src/components/User/Content/RightContent.jsx
@@ -1,9 +1,41 @@
 import CountDown from './CountDown';
+import { useRef } from 'react';
+import { OtherHouses } from '@mui/icons-material';
 
 const RightContent = (props) => {
+    const refDiv = useRef([]);
     const { dataQuiz } = props;
     const onTImeUp = () => {
         props.handleFinish();
+    };
+
+    const getClassQuestion = (index, question) => {
+        //check answers
+        if (question && question.answers.length > 0) {
+            let isAnswered = question.answers.find((a) => a.isSelected === true);
+            if (isAnswered) {
+                return 'question selected';
+            }
+        }
+        return 'question';
+    };
+
+    const handleClickQuestion = (question, index) => {
+        if (refDiv.current) {
+            refDiv.current.forEach((item) => {
+                if (item && item.className === 'question clicked') {
+                    item.className = 'question';
+                }
+            });
+        }
+        if (question && question.answers.length > 0) {
+            let isAnswered = question.answers.find((a) => a.isSelected === true);
+            if (isAnswered) {
+                return;
+            }
+        }
+        refDiv.current[index].className = 'question clicked';
+        props.setIndex(index);
     };
     return (
         <>
@@ -15,7 +47,12 @@ const RightContent = (props) => {
                     dataQuiz.length > 0 &&
                     dataQuiz.map((item, index) => {
                         return (
-                            <div className="question" key={index}>
+                            <div
+                                className={getClassQuestion(index, item)}
+                                key={index}
+                                onClick={() => handleClickQuestion(item, index)}
+                                ref={(element) => (refDiv.current[index] = element)}
+                            >
                                 {index + 1}
                             </div>
                         );
